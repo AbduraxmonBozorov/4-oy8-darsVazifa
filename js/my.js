b235b6f4d6e5142794ed99792ba36614827d9bdf
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-06-01 22:54:33 +0500

    4-oy 8-dars vazifasi yuklandi

---
+++ js/my.js
@@ -0,0 +1,23 @@
+// 1-masala
+// function arrSum(arg){
+//     let sum=0;
+//     for(let i=0; i<arg.length; i++){
+//         sum+=arg[i];
+//     }
+//     return sum;
+// }
+// console.log(arrSum([1, 2, 3, 4, 5]));
+
+
+
+
+// 2-masala
+function arrMax(arg){
+    let max=0;
+    for(let i=0; i<arg.length; i++){
+        console.log(arg[i]);
+    }
+    return max;
+}
+
+console.log(arrMax([1, 2, 10, 3, 2,  5]));
